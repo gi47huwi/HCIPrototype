a34598a48137993322c3fe62f82d063d777ece2f
Andreas Wagner <andiwagner98@outlook.de>
2022-12-01 17:36:21 +0100

    selections

--- prototype/src/components/Configuration.js
+++ prototype/src/components/Configuration.js
@@ -3,12 +3,63 @@ import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import { MDBAccordion, MDBAccordionItem, MDBBtn } from 'mdb-react-ui-kit';
+import {FiSmartphone} from 'react-icons/fi';
+import Slider from '@mui/material/Slider';
+import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 
 function Configuration({
     templateList
 }) {
     const {id} = useParams();
+
+    const [config, setConfig] = useState({
+        case: null,
+        size:null,
+        cpu:null,
+        battery:null
+    })
+
+    const [price, setPrice] = useState(0)
+
+    const [cpu, setCPU] = useState([
+        {
+            name:"Intel i9",
+            performance: "high",
+            price: 400
+        },
+        {
+            name:"AMD Ryzen5",
+            performance: "high",
+            price: 300
+        },
+        {
+            name:"Intel i5",
+            performance: "middle",
+            price: 200
+        }
+    ])
+
+    const [battery, setBattery] = useState([
+        {
+            name:"XYZ",
+            performance: "high",
+            price: 100
+        },
+        {
+            name:"ABC",
+            performance: "middle",
+            price: 80
+        },
+        {
+            name:"LMN",
+            performance: "middle",
+            price: 50
+        }
+    ])
 
     const [options, setOptions] = useState([
         {
@@ -18,15 +69,45 @@ function Configuration({
             children:[
                 {
                     childId:0,
-                    childName:"classic"
+                    childName:"classic",
+                    component:
+                        <MDBBtn 
+                        name={'case_50'}
+                        value={"classic"}
+                        
+                        onClick={(e)=>setData(e)} 
+                        outline 
+                        color="primary" 
+                        className="btn mr-2">
+                            <FiSmartphone className="unclickable"/>
+                            <p className="unclickable">classic</p>
+                        </MDBBtn>
                 },
                 {
                     childId:1,
-                    childName:"foldable"
+                    childName:"foldable",
+                    component:
+                        <MDBBtn 
+                        name='case_200'
+                        value={"foldable"}
+
+                        onClick={(e)=>setData(e)} outline color="primary" className="btn mr-2">
+                            <FiSmartphone className="unclickable" />
+                            <p className="unclickable">foldable</p>
+                        </MDBBtn>
                 },
                 {
                     childId:0,
-                    childName:"keyboard"
+                    childName:"keyboard",
+                    component:
+                        <MDBBtn 
+                        name='case_100'
+                        value={"keyboard"}
+
+                        onClick={(e)=>setData(e)} outline color="primary" className="btn mr-2">
+                            <FiSmartphone className="unclickable"/>
+                            <p className="unclickable">keyboard</p>
+                        </MDBBtn>
                 }
             ]
         },
@@ -38,15 +119,20 @@ function Configuration({
             children:[
                 {
                     childId:0,
-                    childName:"classic"
-                },
-                {
-                    childId:1,
-                    childName:"foldable"
-                },
-                {
-                    childId:0,
-                    childName:"keyboard"
+                    childName:"sizeslider",
+                    component:
+                        <Slider
+                            aria-label="Temperature"
+                            defaultValue={30}
+                            valueLabelDisplay="auto"
+                            name="size"
+                            step={10}
+                            marks
+                            min={10}
+                            max={110}
+                            onChange={(e)=>setData(e)}
+                            />
+                    
                 }
             ]
         },
@@ -58,15 +144,47 @@ function Configuration({
             children:[
                 {
                     childId:0,
-                    childName:"classic"
-                },
-                {
-                    childId:1,
-                    childName:"foldable"
-                },
-                {
-                    childId:0,
-                    childName:"keyboard"
+                    childName:"cpu",
+                    component:
+                    <>
+                    <MDBTable>
+                        <MDBTableHead>
+                            <tr>
+                            <th scope='col'>#</th>
+                            <th scope='col'>Name</th>
+                            <th scope='col'>Performance</th>
+                            <th scope='col'>Price</th>
+                            </tr>
+                        </MDBTableHead>
+                        <MDBTableBody>
+                            
+                                {cpu.map((element)=>{
+                                    return(
+                                        <tr>
+                                        <th scope='row'>
+                                        <Form.Check 
+                                        name={'cpu'+"_"+element.price}
+                                        onClick={(e)=>setData(e)}
+                                        value={element.name}
+
+                                        />
+                                            </th>
+                                        <td>{element.name}</td>
+                                        <td>{element.performance}</td>
+                                        <td>{element.price}</td>
+                                        </tr>
+
+                                    )
+                                })}
+                            
+                        </MDBTableBody>
+                        </MDBTable>
+                        <MDBBtn name="cpu" color='info' onClick={(e)=>setData(e)}>
+                            Select
+                        </MDBBtn>
+                    </>
+
+                        
                 }
             ]
         },
@@ -78,24 +196,87 @@ function Configuration({
             children:[
                 {
                     childId:0,
-                    childName:"classic"
+                    childName:"batterylife",
+                    component:
+                    <>
+                    <MDBTable>
+                    <MDBTableHead>
+                        <tr>
+                        <th scope='col'>#</th>
+                        <th scope='col'>Name</th>
+                        <th scope='col'>Performance</th>
+                        <th scope='col'>Price</th>
+                        </tr>
+                    </MDBTableHead>
+                    <MDBTableBody>
+                            {battery.map((element)=>{
+                                return(
+                                    <tr>
+                                    <th scope='row'>
+                                        <Form.Check 
+                                        name={'battery'+"_"+element.price}
+                                        onClick={(e)=>setData(e)}
+                                        value={element.name}
+                                        
+                                        />
+                                        </th>
+                                    <td>{element.name}</td>
+                                    <td>{element.performance}</td>
+                                    <td>{element.price}</td>
+                                    </tr>
+
+                                )
+                            })}
+                        
+                    </MDBTableBody>
+                    </MDBTable>
+                    <MDBBtn name="battery" color='info' onClick={(e)=>setData(e)}>
+                        Select
+                    </MDBBtn>
+                    </>
                 },
-                {
-                    childId:1,
-                    childName:"foldable"
-                },
-                {
-                    childId:0,
-                    childName:"keyboard"
-                }
+                
             ]
         }
     ])
+    
+
+    const setData = (e) => {
+        console.log(e.target)
+        var data = e.target.name.split("_");
+        console.log(data)
+        setConfig(prev=>({
+            ...prev,
+            [data[0]]: ""+e.target.value
+        }))
+        if(data.length > 1){
+            
+            if(config[data[0]]!==null){
+                setPrice(prev=>(prev + parseInt(data[1])));
+            }else{
+                setPrice(prev=>(prev + parseInt(data[1])));
+
+            }
+
+
+        }
+    }
 
   return (
     <div className="panel">
         <Row style={{height:"90vh"}}>
-            <Col md={6} style={{backgroundColor:"grey"}}>
+            <Col md={6} style={{backgroundColor:"grey", padding:"50px 50px 50px 50px"}}>
+                {Object.keys(config).map((element)=>{
+                    if(config[element]){
+                        return(
+                            <li>
+                                {config[element]}
+                            </li>
+                        )
+                    }
+                    
+                })}
+
                 
             </Col>
             <Col md={6} style={{backgroundColor:"lightgrey"}}>
@@ -109,22 +290,27 @@ function Configuration({
                     options.map((element)=>{
                         return(
                             <MDBAccordionItem collapseId={element.id} headerTitle={element.name + (element.required ? " *" : "") }>
-                                {element.children&&
-                                element.children.map((child)=>{
+                                <Container>
+                                {element.children.map((child)=>{
                                     return(
-                                        <MDBBtn>
-                                            {child.childName}
-                                        </MDBBtn>
+                                        <>
+                                        {child.component}
+                                        </>
 
                                     )
                                 })
                                 }
+                                </Container>
                             </MDBAccordionItem>
                         )
 
                     })
                 }
                 </MDBAccordion>
+
+                <Alert className="price" variant='danger'>
+                    {price}
+                </Alert>
 
                 
             </Col>
